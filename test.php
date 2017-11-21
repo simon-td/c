57cdd3b79bd46e91d60eaa6a018cd2e91d326052
simon-td <simon@tradedepot.com>
2017-11-21 15:05:17 +1300

    add test text

--- test.php
+++ test.php
@@ -10,7 +10,7 @@ $dbname = "test";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
+// Check connection test
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
